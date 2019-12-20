aa515eca5a2a07f0605bea14538e046fb0290c52
phi0411141 <hongphipham97@gmail.com>
2019-12-20 09:47:18 +0000

    Update Server.py

--- Server.py
+++ Server.py
@@ -8,8 +8,10 @@ from sklearn.svm import LinearSVC
 import string
 import numpy as np
 from datetime import timedelta
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
@@ -158,57 +160,6 @@ def test(s):
         new_pre_id = clf2.predict([X])[0]
         return categories[new_pre_id]
 
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
-                attach_to_all=True, automatic_options=True):
-    """Decorator function that allows crossdomain requests.
-      Courtesy of
-      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
-    """
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, str):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, str):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        """ Determines which methods are allowed
-        """
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        """The decorator function
-        """
-        def wrapped_function(*args, **kwargs):
-            """Caries out the actual cross domain code
-            """
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
 
 @app.route('/')
 def hello():
@@ -216,7 +167,6 @@ def hello():
 
 
 @app.route('/', methods=['POST'])
-@crossdomain(origin='*')
 def category_guess():
     # client = MongoClient()
     # db = client.admin
